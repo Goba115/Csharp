5d87ebaac19dc924857c2efd61c5b9416fdff7bb
Gabrijel Peternel <102869281+Goba115@users.noreply.github.com>
2024-02-22 22:07:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿void Izpis() //izpiše vsako črko posebi in na koncu pove število znakov
+{
+    string vhod;
+    Console.WriteLine("vpišite besedo:");
+    vhod = Console.ReadLine();
+
+    string resnica = vhod;
+    int dolzina = vhod.Length;
+
+    Console.WriteLine("\n");
+
+    for (int i = 0; i < dolzina; i++)
+    {
+        Console.WriteLine(vhod[i]);
+    }
+
+    Console.WriteLine("\n" + dolzina);
+}
+
+
+Izpis();
+Console.ReadLine();
